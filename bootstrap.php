3c35b17c3f346718e9ac7b261e078f8a759a1659
Casey <casey@gigaom.com>
2012-11-05 23:41:14 +0000

    moving the config files into a dir named local, renaming the bootstrap file for VIP to conform, removing the logic that looked at the hostname to determine what config file to load and specifying it in go-channels instead

---
+++ bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: GO Crosspost
+ * Plugin URI: http://gigaom.com
+ * Description: Pings another domain to let it know a new relevant post has been added which
+ *              triggers that domain to then pull the data it needs to replicate the post in WP.
+ * Author: GigaOM
+ * Version: 1.0
+ * Author URI: http://gigaom.com/
+ */
+
+require_once __DIR__ . '/components/class-go-xpost-migrator.php';
+require_once __DIR__ . '/components/class-go-xpost.php';
+require_once __DIR__ . '/components/functions.php';
+
+global $goxpost;
+
+// load the business logic we're using and configure
+require_once __DIR__ .'/local/class-go-xpost-'.  go_channel()->go_xpost_config .'.php';
+$go_xpost_class_name = 'GO_XPost_'. ucfirst( go_channel()->go_xpost_config );
+$goxpost = new $go_xpost_class_name( go_channel()->go_xpost_config_url );
+unset( $go_xpost_class_name );
