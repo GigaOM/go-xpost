83af2d084029916fb7a36eeb15fd8d79716faabc
Matthew Batchelder <borkweb@gmail.com>
2012-11-08 13:33:37 -0500

    Don't try to include a non-existent file.

--- bootstrap.php
+++ bootstrap.php
@@ -11,7 +11,6 @@
 
 require_once __DIR__ . '/components/class-go-xpost-migrator.php';
 require_once __DIR__ . '/components/class-go-xpost.php';
-require_once __DIR__ . '/components/functions.php';
 
 global $goxpost;
 
