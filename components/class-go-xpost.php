6bc3a75b9f156263921190fcffee47112acc7145
Matthew Batchelder <borkweb@gmail.com>
2013-04-26 13:36:30 -0400

    Spacing improvements

--- components/class-go-xpost.php
+++ components/class-go-xpost.php
@@ -192,14 +192,14 @@ class GO_XPost
 	 */
 	public function load_filter( $filter )
 	{
-		if ( ! isset( $this->filters[$filter] ) )
+		if ( ! isset( $this->filters[ $filter ] ) )
 		{
 			include_once dirname( __DIR__ ) . '/filters/class-go-xpost-filter-' . $filter . '.php';
 			$classname = 'GO_XPost_Filter_' . ucfirst( $filter );
 
 			if ( class_exists( $classname ) )
 			{
-				$this->filters[$filter] = new $classname;
+				$this->filters[ $filter ] = new $classname;
 			}//end if
 		}// end if
 	} // END load_filter
