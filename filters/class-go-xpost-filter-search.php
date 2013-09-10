a5d367c0e3648e49ee8cd7cf77cdb78c01387a25
Casey Bisson <casey@gigaom.com>
2013-09-10 22:03:33 +0000

    cleanup and simplification, plus some improvements

--- filters/class-go-xpost-filter-search.php
+++ filters/class-go-xpost-filter-search.php
@@ -16,7 +16,7 @@ class GO_XPost_Filter_Search extends GO_XPost_Filter
 	{
 		$post = get_post( $post_id );
 
-		// exclude sponsor posts (does not apply to underwritten reports)
+		// exclude sponsor posts (we only filter this on GO/pC / does not apply to underwritten reports)
 		if ( apply_filters( 'go_sponsor_posts_is_sponsor_post', FALSE, $post_id ) )
 		{
 			return FALSE;
@@ -53,8 +53,11 @@ class GO_XPost_Filter_Search extends GO_XPost_Filter
 		)
 		{
 			if (
-				0 === stripos( $post->post_title, 'about ' ) && // match "About GigaOM Pro" and "About Keren Elazari"
-				5 < $post->menu_order // exclude early sections, which might begin with "about" for other reasons
+				0 === stripos( $post->post_title, 'about gigaom' ) || // match "About GigaOM..."
+				(
+					0 === stripos( $post->post_title, 'about ' ) && // match "About Keren Elazari"
+					5 < $post->menu_order // exclude early sections, which might begin with "about" for other reasons
+				)
 			)
 			{
 				return FALSE;
@@ -69,7 +72,7 @@ class GO_XPost_Filter_Search extends GO_XPost_Filter
 				'poll-summaries', // Same for poll summaries
 			);
 
-			$categories = wp_get_object_terms( $post_id, array( 'category' ), array( 'fields' => 'slugs' ) );
+			$categories = get_the_terms( $post_id, array( 'category' ), array( 'fields' => 'slugs' ) );
 
 			foreach ( $categories as $category )
 			{
@@ -99,93 +102,40 @@ class GO_XPost_Filter_Search extends GO_XPost_Filter
 		$post = $xpost->post;
 		$post->ID = $post_id;
 
-		// Will set this later to Subscription if appropriate
+		// shorten the post excerpt if we have one (preserve the full excerpt in the post content, so we can still search it)
+		if ( ! empty( $xpost->post->post_excerpt ) )
+		{
+			$xpost->post->post_content = $xpost->post->post_excerpt . "\n\n" . $xpost->post->post_content;
+			$xpost->post->post_excerpt = trim( wp_trim_words( $xpost->post->post_content, 31, '' ), '.' ) . '&hellip;';
+		}
+		// or generate a post excerpt if we don't have one (saves us from having to generate it when displaying the posts)
+		else
+		{
+			$xpost->post->post_excerpt = trim( wp_trim_words( $xpost->post->post_content, 31, '' ), '.' ) . '&hellip;';
+		}
+
+		// special handling for excerpts on link posts
+		// this only runs on GO/pC for now
+		if ( class_exists( 'GO_Theme_Post' ) )
+		{
+			$go_post = new GO_Theme_Post( $post );
+			if ( $go_post->is_type( 'link' ) )
+			{
+				$content = preg_replace( $go_post->link_pattern, '', $content, 1 );
+				$xpost->post->post_excerpt = trim( wp_trim_words( $content, 31, '' ), '.' ) . '&hellip;';
+			}
+
+			unset( $go_post );
+		}
+
+		// Will change this to Subscription later if appropriate
 		$availability = 'Free';
 
 		// go-property will come from the current property set in go_config
 		$xpost->terms['go-property'][] = go_config()->get_property();
 
-		// vertical can potentially come from vertical, channel, primary_channel, and category -> child of Topics
-		if ( isset( $xpost->terms['channel'] ) )
-		{
-			foreach ( $xpost->terms['channel'] as $channel )
-			{
-				$xpost->terms['vertical'][] = ucwords( $channel );
-			} // END foreach
-		} // END if
-
-		if ( isset( $xpost->terms['primary_channel'] ) )
-		{
-			foreach ( $xpost->terms['primary_channel'] as $primary_channel )
-			{
-				$xpost->terms['vertical'][] = ucwords( $primary_channel );
-			} // END foreach
-		} // END if
-
-		if ( isset( $xpost->terms['vertical'] ) )
-		{
-			$xpost->terms['vertical'] = array_unique( $xpost->terms['vertical'] );
-		} // END if
-
-		// This can be deleted once category to vertical stuff has launched
-		if ( isset( $xpost->terms['category'] ) )
-		{
-			$topics_term     = get_term_by( 'name', 'Topics', 'category' );
-			$topics_children = get_terms( array( 'category' ), array( 'child_of' => $topics_term->term_id ) );
-			$topics_children = $this->parse_terms_array( $topics_children );
-
-			foreach ( $xpost->terms['category'] as $category )
-			{
-				if ( in_array( $category, $topics_children ) )
-				{
-					$xpost->terms['vertical'][] = $category;
-				} // END if
-			} // END foreach
-		} // END if
-
 		// go-type is the fun one, it will come a variety of sources
 		$xpost->terms['go-type'] = array();
-
-		// This will determine the go-type value for Pro reports and shortposts
-		// it's specific to those post types to avoid messing with charts or webinars
-		if (
-			isset( $xpost->terms['category'] ) &&
-			(
-				0 == strncmp( 'go-report', $xpost->post->post_type, 9 ) ||
-				'go_shortpost' == $xpost->post->post_type
-			)
-		)
-		{
-			foreach ( $xpost->terms['category'] as $category )
-			{
-				if (
-					'Briefings' == $category
-					|| 'Research Briefings' == $category
-					|| 'Research Notes' == $category
-					|| 'Long Views' == $category
-				)
-				{
-					$xpost->terms['go-type'][] = 'Report';
-					$availability = 'Subscription';
-				}
-				elseif ( 'Sector Roadmaps' == $category )
-				{
-					$xpost->terms['go-type'][] = 'Sector Roadmap';
-					$availability = 'Subscription';
-				} // END elseif
-				elseif ( 'Quarterly Wrap Ups' == $category )
-				{
-					$xpost->terms['go-type'][] = 'Quarterly Wrap Up';
-					$availability = 'Subscription';
-				} // END elseif
-			} // END foreach
-
-			// If none of the above were triggered we've got a Blog Post
-			if ( empty( $xpost->terms['go-type'] ) )
-			{
-				$xpost->terms['go-type'][] = 'Blog Post';
-			} // END if
-		} // END if
 
 		if ( 'go-datamodule' == $xpost->post->post_type )
 		{
@@ -216,6 +166,38 @@ class GO_XPost_Filter_Search extends GO_XPost_Filter
 		} // END elseif
 		elseif ( 0 == strncmp( 'go-report', $xpost->post->post_type, 9 ) )
 		{
+			// set the availability up top
+			// we may need to recnosider this if we use the report plugin on Gigaom or other non-subscriber sites
+			$availability = 'Subscription';
+
+			// @TODO: this will need to be refactored for R.go and the new taxonomy
+			// Sector Roadmaps and Quarterly Wrap Ups are special
+			if ( isset( $xpost->terms['category'] ) )
+			{
+				foreach ( $xpost->terms['category'] as $category )
+				{
+					switch ( strtolower( $category ) )
+					{
+						case 'sector roadmaps':
+							$xpost->terms['go-type'][] = 'Sector Roadmap';
+							break;
+						case 'quarterly wrap ups':
+						case 'quarterly wrap-ups':
+							$xpost->terms['go-type'][] = 'Quarterly Wrap-Up';
+							break;
+
+					} // END switch
+				} // END foreach
+			} // END if
+
+			// Catch other types of reports, including, but not limited to:
+			// briefings, research briefings, research notes, long views
+			// at some point it may be necessary to tweak this if the report plugin ever gets used elsewhere
+			if ( ! count( $xpost->terms['go-type'] ) )
+			{
+				$xpost->terms['go-type'][] = 'Report';
+			}
+
 			// this is a report front page
 			if ( 'go-report' == $xpost->post->post_type )
 			{
@@ -251,20 +233,33 @@ class GO_XPost_Filter_Search extends GO_XPost_Filter
 				unset( $xpost->parent );
 			} // END elseif
 
-			// At some point it may be necessary to tweak how these next 6 lines work if the report plugin ever gets used elsewhere
-			if ( ! in_array( 'Report', $xpost->terms['go-type'] ) )
-			{
-				$xpost->terms['go-type'][] = 'Report';
-			} // END if
-
-			$availability = 'Subscription';
 		} // END elseif
 		elseif ( 'go_shortpost' == $xpost->post->post_type )
 		{
 			$xpost->terms['go-type'][] = 'Blog Post';
 		}// end elseif
-		elseif ( function_exists( 'go_waterfall_options' ) ) // or maybe go_config()->dir != '_pro' at some point?
+		if ( in_array( go_config()->get_property_slug(), array( 'gigaom', 'paidcontent' ) ) )
 		{
+			// GigaOM and paidContent channels are transformed into verticals
+			// Pro has a channels taxonomy, but it includes a lot of accidental noise and isn't used
+			if ( isset( $xpost->terms['channel'] ) )
+			{
+				foreach ( $xpost->terms['channel'] as $channel )
+				{
+					$xpost->terms['vertical'][] = ucwords( $channel );
+				} // END foreach
+			} // END if
+
+			if ( isset( $xpost->terms['primary_channel'] ) )
+			{
+				foreach ( $xpost->terms['primary_channel'] as $primary_channel )
+				{
+					$xpost->terms['vertical'][] = ucwords( $primary_channel );
+				} // END foreach
+			} // END if
+
+			// get the post content type
+			// frustratingly, we have to look in multiple places for this
 			if (
 				'video' == go_waterfall_options()->get_type( $post_id )
 				|| ( isset( $xpost->terms['go_syn_media'] ) && in_array( 'video', $xpost->terms['go_syn_media'] ) )
@@ -279,10 +274,7 @@ class GO_XPost_Filter_Search extends GO_XPost_Filter
 			{
 				$xpost->terms['go-type'][] = 'Audio';
 			} // END elseif
-			elseif (
-				'gigaom' == go_waterfall_options()->get_type( $post_id )
-				|| 'paidcontent' == go_waterfall_options()->get_type( $post_id )
-			)
+			else
 			{
 				$xpost->terms['go-type'][] = 'Blog Post';
 			} // END elseif
@@ -306,7 +298,7 @@ class GO_XPost_Filter_Search extends GO_XPost_Filter
 		// search does not need the thumbnails
 		foreach( $xpost->meta as $meta_key => $meta_values )
 		{
-			if ( strpos( $meta_key, '_thumbnail_id' ) !== FALSE )
+			if ( FALSE !== strpos( $meta_key, '_thumbnail_id' ) )
 			{
 				unset( $xpost->meta[ $meta_key ] );
 			}// end if
@@ -318,6 +310,9 @@ class GO_XPost_Filter_Search extends GO_XPost_Filter
 		// Remove any post_format if it exists
 		unset( $xpost->terms['post_format'] );
 
+		// don't send any category terms to search
+		unset( $xpost->terms['category'] );
+
 		// Force all post types to be a post for search
 		$xpost->post->post_type = 'post';
 
@@ -327,21 +322,4 @@ class GO_XPost_Filter_Search extends GO_XPost_Filter
 		return $xpost;
 	} // END post_filter
 
-	/**
-	 * Return a simplified array of terms
-	 */
-	public function parse_terms_array( $terms )
-	{
-		$parsed_terms = array();
-
-		if ( is_array( $terms ) )
-		{
-			foreach ( $terms as $term )
-			{
-				$parsed_terms[] = $term->name;
-			} // END foreach
-		} // END if
-
-		return $parsed_terms;
-	} // END parse_terms_array
 } // END GO_XPost_Filter_Search
