d61fecacc43a1231f39ce6d2949928f5f983b4f1
Zach Tirrell <zbtirrell@gmail.com>
2014-06-26 15:17:22 -0400

    added chart-units and chart-type to data xposted to search for go-datamodule

--- filters/class-go-xpost-filter-search.php
+++ filters/class-go-xpost-filter-search.php
@@ -369,6 +369,14 @@ class GO_XPost_Filter_Search extends GO_XPost_Filter
 		{
 			// set the type and availability
 			$xpost->terms['go-type'][] = 'Chart';
+			$xpost->terms['chart-type'][] = $xpost->meta['data_set_v3']['settings']['graph_type'];
+			$units = wp_get_object_terms( $xpost->post->ID, 'go-datamodule-units' );
+			foreach ( $units as $unit_term )
+			{
+				$xpost->terms['chart-units'][] = $unit_term->name;
+			}//end foreach
+			unset( $xpost->terms['go-datamodule-units'] );
+
 			$availability = 'Subscription';
 
 			// remove the parent ID and object
